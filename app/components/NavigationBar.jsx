6f3698973af2d0ba722945cd3063bd627a724a44
Alexandru Stelea <alexandrustelea@yahoo.com>
2023-11-25 18:11:07 +0200

    Created navigation bar component.

---
+++ app/components/NavigationBar.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import styles from "./navigationBar.module.css";
+
+export default function NavigationBar() {
+  return (
+    <div className={styles.navbar}>
+    <div className={styles.navbarLeft}>
+      <button className={styles.homeButton}>Home</button>
+    </div>
+    <div className={styles.navbarTitle}>
+      <h1>City Gazer </h1>
+    </div>
+    <div className={styles.navbarRight}>
+      <button className={styles.contactButton}>Contact</button>
+    </div>
+  </div>
+  );
+}
