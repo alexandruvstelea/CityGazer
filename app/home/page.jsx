75118d481393051ad00fd6f7a924e974e200d13b
Alexandru Stelea <alexandrustelea@yahoo.com>
2023-11-25 18:11:28 +0200

    Created home page.

---
+++ app/home/page.jsx
@@ -0,0 +1,12 @@
+import styles from "./page.module.css";
+import NavigationBar from "../components/NavigationBar";
+import Footer from "../components/Footer.jsx"
+
+export default function LandingPage() {
+  return (
+    <>
+    <NavigationBar/>
+    <Footer text="Developed By Alexandru Stelea"/>
+    </>
+  );
+}
