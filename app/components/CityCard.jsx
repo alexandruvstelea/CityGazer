6c58b91fefd323d35dd6bb1e8c66454e8a2d134a
Alexandru Stelea <alexandrustelea@yahoo.com>
2023-11-26 15:54:46 +0200

    Created react city card component.

---
+++ app/components/CityCard.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+import styles from "./cityCard.module.css";
+import Image from "next/image";
+
+function CityCard({ city, country, imageUrl }) {
+  return (
+    <div className={styles.cityCard}>
+      <div className={styles.cardImage}>
+        <Image
+          className={styles.cityImage}
+          src={imageUrl}
+          alt={city}
+          fill={true}
+          quality={100}
+          priority={true}
+        />
+      </div>
+      <div className={styles.cityInfo}>
+        <h2>{city}</h2>
+        <p>{country}</p>
+        <button className={styles.viewMoreBtn}>Gaze city</button>
+      </div>
+    </div>
+  );
+}
+
+export default CityCard;
