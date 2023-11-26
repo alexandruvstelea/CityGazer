a0aaeb565e6fe47c28db6e628d372f18d423ffb9
Alexandru Stelea <alexandrustelea@yahoo.com>
2023-11-26 15:51:49 +0200

    Started creating design for homepage.

--- app/home/page.jsx
+++ app/home/page.jsx
@@ -1,12 +1,36 @@
+"use client";
 import styles from "./page.module.css";
 import NavigationBar from "../components/NavigationBar";
-import Footer from "../components/Footer.jsx"
+import CityCard from "../components/CityCard";
+import Footer from "../components/Footer.jsx";
+import SearchBar from "../components/SearchBar";
 
 export default function LandingPage() {
   return (
     <>
-    <NavigationBar/>
-    <Footer text="Developed By Alexandru Stelea"/>
+      <NavigationBar />
+      <div className={styles.searchSection}>
+        <SearchBar />
+      </div>
+      <div className={styles.cardSection}>
+        <CityCard city="Brasov" country="Romania" imageUrl="/brasov.jpg" />
+        <CityCard city="Bacau" country="Romania" imageUrl="/bacau.jpg" />
+        <CityCard city="Cluj" country="Romania" imageUrl="/cluj.jpeg" />
+        <CityCard city="Galati" country="Romania" imageUrl="/galati.jpg" />
+        <CityCard city="Iasi" country="Romania" imageUrl="/iasi.jpg" />
+        <CityCard city="Oradea" country="Romania" imageUrl="/oradea.jpg" />
+        <CityCard
+          city="Bucuresti"
+          country="Romania"
+          imageUrl="/bucuresti.jpeg"
+        />
+        <CityCard
+          city="Timisoara"
+          country="Romania"
+          imageUrl="/timisoara.jpg"
+        />
+      </div>
+      <Footer text="Developed By Alexandru Stelea" />
     </>
   );
 }
