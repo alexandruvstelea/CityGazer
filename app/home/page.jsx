2a94b824551572744041ebd1aec1e3120d74757f
alexandruvstelea <alexandrustelea@yahoo.com>
2023-11-26 17:56:55 +0200

    Created function to map cities to cards.

--- app/home/page.jsx
+++ app/home/page.jsx
@@ -5,32 +5,40 @@ import CityCard from "../components/CityCard";
 import Footer from "../components/Footer.jsx";
 import SearchBar from "../components/SearchBar";
 
-export default function LandingPage() {
+const cities = [
+    { city: 'Brasov', country: 'Romania', imageUrl: '/brasov.jpg' },
+    { city: 'Bacau', country: 'Romania', imageUrl: '/bacau.jpg' },
+    { city: 'Bucuresti', country: 'Romania', imageUrl: '/bucuresti.jpeg' },
+    { city: 'Cluj', country: 'Romania', imageUrl: '/cluj.jpeg' },
+    { city: 'Galati', country: 'Romania', imageUrl: '/galati.jpg' },
+    { city: 'Iasi', country: 'Romania', imageUrl: '/iasi.jpg' },
+    { city: 'Oradea', country: 'Romania', imageUrl: '/oradea.jpg' },
+    { city: 'Timisoara', country: 'Romania', imageUrl: '/timisoara.jpg' },
+  ];
+
+function HomePage() {
   return (
     <>
       <NavigationBar />
       <div className={styles.searchSection}>
         <SearchBar />
       </div>
+      <div className={styles.header}>
+        <h1>Explore this cities</h1>
+      </div>
       <div className={styles.cardSection}>
-        <CityCard city="Brasov" country="Romania" imageUrl="/brasov.jpg" />
-        <CityCard city="Bacau" country="Romania" imageUrl="/bacau.jpg" />
-        <CityCard city="Cluj" country="Romania" imageUrl="/cluj.jpeg" />
-        <CityCard city="Galati" country="Romania" imageUrl="/galati.jpg" />
-        <CityCard city="Iasi" country="Romania" imageUrl="/iasi.jpg" />
-        <CityCard city="Oradea" country="Romania" imageUrl="/oradea.jpg" />
-        <CityCard
-          city="Bucuresti"
-          country="Romania"
-          imageUrl="/bucuresti.jpeg"
-        />
-        <CityCard
-          city="Timisoara"
-          country="Romania"
-          imageUrl="/timisoara.jpg"
-        />
+       {cities.map((cityData, index) => (
+          <CityCard
+            key={index}
+            city={cityData.city}
+            country={cityData.country}
+            imageUrl={cityData.imageUrl}
+          />
+        ))}
       </div>
       <Footer text="Developed By Alexandru Stelea" />
     </>
   );
 }
+
+export default HomePage;
