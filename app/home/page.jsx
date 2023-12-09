e5217529378425c831927754326ca4b54d28a445
Alexandru Stelea <alexandrustelea@yahoo.com>
2023-12-09 04:15:24 +0200

    Finished implementing search function.

--- app/home/page.jsx
+++ app/home/page.jsx
@@ -1,42 +1,108 @@
 "use client";
+import React, { useState } from "react";
 import styles from "./page.module.css";
 import NavigationBar from "../components/NavigationBar";
 import CityCard from "../components/CityCard";
-import Footer from "../components/Footer.jsx";
+import Footer from "../components/Footer";
 import SearchBar from "../components/SearchBar";
+import cities from "./startCities";
 
-const cities = [
-  { city: "Brasov", country: "Romania", imageUrl: "/brasov.jpg" },
-  { city: "Bacau", country: "Romania", imageUrl: "/bacau.jpg" },
-  { city: "Bucuresti", country: "Romania", imageUrl: "/bucuresti.jpeg" },
-  { city: "Cluj", country: "Romania", imageUrl: "/cluj.jpeg" },
-  { city: "Galati", country: "Romania", imageUrl: "/galati.jpg" },
-  { city: "Iasi", country: "Romania", imageUrl: "/iasi.jpg" },
-  { city: "Oradea", country: "Romania", imageUrl: "/oradea.jpg" },
-  { city: "Timisoara", country: "Romania", imageUrl: "/timisoara.jpg" },
-  { city: "Alba Iulia", country: "Romania", imageUrl: "/alba.jpg" },
-  { city: "Hunedoara", country: "Romania", imageUrl: "/hunedoara.jpg" },
-  { city: "Sibiu", country: "Romania", imageUrl: "/sibiu.jpeg" },
-  { city: "Suceava", country: "Romania", imageUrl: "/suceava.jpeg" },
-];
+async function formatCityData(data, searchTerm) {
+  const formattedCities = data
+    .map((city) => {
+      const parts = city.matching_full_name.split(",");
+      if (parts.length >= 2) {
+        const cityName = parts[0].trim();
+        const regionName = `${parts[parts.length - 2]},${
+          parts[parts.length - 1]
+        }`
+          .trim()
+          .replace(/ *\([^)]*\) */g, "");
+        if (
+          cityName
+            .toLowerCase()
+            .normalize("NFD")
+            .replace(/[\u0300-\u036f]/g, "")
+            .includes(searchTerm.toLowerCase())
+        )
+          return {
+            name: cityName,
+            region: regionName,
+            fullName: city.matching_full_name,
+          };
+      }
+      return null;
+    })
+    .filter(Boolean);
+  return formattedCities;
+}
+
+async function addCityImages(data) {
+  const promises = data.map(async (city) => {
+    try {
+      const response = await fetch(
+        `https://api.teleport.org/api/urban_areas/slug:${city.name
+          .toLowerCase()
+          .replace(/\s+/g, "-")}/images/`
+      );
+      if (!response.ok) {
+        throw new Error("Network response was not ok");
+      }
+      const data = await response.json();
+      city.imageUrl = data["photos"][0]["image"]["web"];
+      console.log(city.imageUrl);
+      return city;
+    } catch (error) {
+      let randomNumber = Math.floor(Math.random() * 4 + 1);
+      city.imageUrl = `/stock_${randomNumber}.png`;
+      return city;
+    }
+  });
+  const fullData = await Promise.all(promises);
+  return fullData;
+}
 
 function HomePage() {
+  const [cityData, setCityData] = useState(cities);
+
+  const fetchCityData = async (searchTerm) => {
+    const resultsLimit = 24;
+    const apiUrl = `https://api.teleport.org/api/cities/?search=${searchTerm.toLowerCase()}&limit=${resultsLimit}`;
+    console.log(apiUrl);
+    try {
+      const response = await fetch(apiUrl);
+      if (!response.ok) {
+        throw new Error("Network response was not ok");
+      }
+      const data = await response.json();
+      const formatedData = await formatCityData(
+        data["_embedded"]["city:search-results"],
+        searchTerm
+      );
+      const fullData = await addCityImages(formatedData);
+      setCityData(fullData);
+    } catch (error) {
+      console.error("There was a problem with the fetch operation:", error);
+    }
+  };
+
   return (
     <>
       <NavigationBar />
       <div className={styles.searchSection}>
-        <SearchBar />
+        <SearchBar onSearch={fetchCityData} />
       </div>
       <div className={styles.header}>
-        <h1>Explore this cities</h1>
+        <h1>Explore these Cities</h1>
       </div>
       <div className={styles.cardSection}>
-        {cities.map((cityData, index) => (
+        {cityData.map((city, index) => (
           <CityCard
             key={index}
-            city={cityData.city}
-            country={cityData.country}
-            imageUrl={cityData.imageUrl}
+            city={city.name}
+            region={city.region}
+            imageUrl={city.imageUrl}
+            fullName={city.fullName}
           />
         ))}
       </div>
