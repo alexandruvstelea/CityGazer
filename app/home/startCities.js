bae572197059f6afdbddbd9a724ca4faa655d358
Alexandru Stelea <alexandrustelea@yahoo.com>
2023-12-09 04:15:03 +0200

    Created separate file for initial city cards.

---
+++ app/home/startCities.js
@@ -0,0 +1,76 @@
+const cities = [
+  {
+    name: "Bucharest",
+    region: "Bucureşti, Romania",
+    imageUrl:
+      "https://d13k13wj6adfdf.cloudfront.net/urban_areas/bucharest_web-b82e0bb355.jpg",
+  },
+  {
+    name: "Madrid",
+    region: "Madrid, Spain",
+    imageUrl:
+      "https://d13k13wj6adfdf.cloudfront.net/urban_areas/madrid_web-baf72ff2d6.jpg",
+  },
+  {
+    name: "Paris",
+    region: "Île-de-France, France",
+    imageUrl:
+      "https://d13k13wj6adfdf.cloudfront.net/urban_areas/paris_web-0a3c7314a5.jpg",
+  },
+  {
+    name: "Cluj-Napoca",
+    region: "Cluj, Romania",
+    imageUrl:
+      "https://d13k13wj6adfdf.cloudfront.net/urban_areas/cluj-napoca_web-5ceb03ea4b.jpg",
+  },
+  {
+    name: "Sofia",
+    region: "Sofia-Capital, Bulgaria",
+    imageUrl:
+      "https://d13k13wj6adfdf.cloudfront.net/urban_areas/sofia_web-f5f214e15a.jpg",
+  },
+  {
+    name: "Moscow",
+    region: "Moscow, Russia",
+    imageUrl:
+      "https://d13k13wj6adfdf.cloudfront.net/urban_areas/moscow_web-3b49a7d2d1.jpg",
+  },
+  {
+    name: "Budapest",
+    region: "Budapest, Hungary",
+    imageUrl:
+      "https://d13k13wj6adfdf.cloudfront.net/urban_areas/budapest_web-9de9bc410f.jpg",
+  },
+  {
+    name: "Vienna",
+    region: "Vienna, Austria",
+    imageUrl:
+      "https://d13k13wj6adfdf.cloudfront.net/urban_areas/vienna_web-b2c460ae31.jpg",
+  },
+  {
+    name: "Los Angeles",
+    region: "California, United States",
+    imageUrl:
+      "https://d13k13wj6adfdf.cloudfront.net/urban_areas/los-angeles_web-2eedb2a83e.jpg",
+  },
+  {
+    name: "Tokyo",
+    region: "Tokyo, Japan",
+    imageUrl:
+      "https://d13k13wj6adfdf.cloudfront.net/urban_areas/tokyo_web-7a20b5733f.jpg",
+  },
+  {
+    name: "Beijing",
+    region: "Beijing, China",
+    imageUrl:
+      "https://d13k13wj6adfdf.cloudfront.net/urban_areas/beijing_web-a6aa65e646.jpg",
+  },
+  {
+    name: "Buenos Aires",
+    region: "Buenos Aires F.D., Argentina",
+    imageUrl:
+      "https://d13k13wj6adfdf.cloudfront.net/urban_areas/buenos-aires_web-17f0020100.jpg",
+  },
+];
+
+export default cities;
