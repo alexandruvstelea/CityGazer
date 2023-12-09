68e7c4ddf2cdc7ead33c89b494b88194b3d7abca
Alexandru Stelea <alexandrustelea@yahoo.com>
2023-12-09 04:17:08 +0200

    Removed console log for the moment.

--- app/components/SearchBar.jsx
+++ app/components/SearchBar.jsx
@@ -1,21 +1,47 @@
-import React, { useState } from "react";
+import React, { useState, useRef, useEffect } from "react";
 import styles from "./searchBar.module.css";
+import { useRouter, useSearchParams } from "next/navigation";
 
-const SearchBar = () => {
+function SearchBar({ onSearch }) {
   const [isFocused, setIsFocused] = useState(false);
+  const searchParams = useSearchParams();
+  const [searchTerm, setSearchTerm] = useState("");
+  const inputRef = useRef(null);
+  const router = useRouter();
+
+  const handleSearch = (e) => {
+    if (e.key === "Enter") {
+      router.push(`/home?search=${encodeURIComponent(searchTerm)}`);
+      inputRef.current && inputRef.current.blur();
+    }
+  };
+
+  useEffect(() => {
+    if (searchParams.get("search")) {
+      // !console.log("useEffect triggered", searchParams.get("search"));
+      setSearchTerm(searchParams.get("search"));
+      onSearch(searchParams.get("search"));
+    } else {
+      setSearchTerm("");
+    }
+  }, [searchParams.get("search")]);
 
   return (
     <div className={`${styles.searchBar} ${isFocused ? styles.focused : ""}`}>
-      <span className={styles.searchIcon}>&#128269;</span> {/* Search icon */}
+      <span className={styles.searchIcon}>&#128269;</span>
       <input
+        ref={inputRef}
         type="text"
         placeholder="Search a city"
+        className={styles.searchInput}
+        value={searchTerm}
+        onChange={(e) => setSearchTerm(e.target.value)}
         onFocus={() => setIsFocused(true)}
         onBlur={() => setIsFocused(false)}
-        className={styles.searchInput}
+        onKeyDown={handleSearch}
       />
     </div>
   );
-};
+}
 
 export default SearchBar;
