d4db86cb4173eb1876c8a5c17838b4d24154c0b7
Alexandru Stelea <alexandrustelea@yahoo.com>
2023-12-11 05:33:20 +0200

    Modified footer to have defaul text and added anchor.

--- app/components/Footer.jsx
+++ app/components/Footer.jsx
@@ -1,6 +1,15 @@
 import React from "react";
 import styles from "./footer.module.css";
 
-export default function AnimatedButton({ text }) {
-  return <div className={styles.footer}>{text}</div>;
+export default function Footer() {
+  const linkedinUrl = "https://www.linkedin.com/in/alexandrustelea/";
+
+  return (
+    <div className={styles.footer}>
+      Developed by{" "}
+      <a href={linkedinUrl} target="_blank">
+        Alexandru Stelea
+      </a>
+    </div>
+  );
 }
