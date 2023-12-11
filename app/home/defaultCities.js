87ea9845a91fcc501c7887cc93b48a831cd21e3f
Alexandru Stelea <alexandrustelea@yahoo.com>
2023-12-11 05:28:45 +0200

    Added geoId to default cities.

--- app/home/defaultCities.js
+++ app/home/defaultCities.js
@@ -1,75 +1,99 @@
 const defaultCities = [
   {
     name: "Bucharest",
-    region: "Bucureşti, Romania",
+    region: "Bucureşti",
+    country: "Romania",
     imageUrl:
       "https://d13k13wj6adfdf.cloudfront.net/urban_areas/bucharest_web-b82e0bb355.jpg",
+    geoId: 683506,
   },
   {
     name: "Madrid",
-    region: "Madrid, Spain",
+    region: "Madrid",
+    country: "Spain",
     imageUrl:
       "https://d13k13wj6adfdf.cloudfront.net/urban_areas/madrid_web-baf72ff2d6.jpg",
+    geoId: 3117735,
   },
   {
     name: "Paris",
-    region: "Île-de-France, France",
+    region: "Île-de-France",
+    country: "France",
     imageUrl:
       "https://d13k13wj6adfdf.cloudfront.net/urban_areas/paris_web-0a3c7314a5.jpg",
+    geoId: 2988507,
   },
   {
     name: "Cluj-Napoca",
-    region: "Cluj, Romania",
+    region: "Cluj",
+    country: "Romania",
     imageUrl:
       "https://d13k13wj6adfdf.cloudfront.net/urban_areas/cluj-napoca_web-5ceb03ea4b.jpg",
+    geoId: 681290,
   },
   {
     name: "Sofia",
-    region: "Sofia-Capital, Bulgaria",
+    region: "Sofia-Capital",
+    country: "Bulgaria",
     imageUrl:
       "https://d13k13wj6adfdf.cloudfront.net/urban_areas/sofia_web-f5f214e15a.jpg",
+    geoId: 727011,
   },
   {
     name: "Moscow",
-    region: "Moscow, Russia",
+    region: "Moscow",
+    country: "Russia",
     imageUrl:
       "https://d13k13wj6adfdf.cloudfront.net/urban_areas/moscow_web-3b49a7d2d1.jpg",
+    geoId: 524901,
   },
   {
     name: "Budapest",
-    region: "Budapest, Hungary",
+    region: "Budapest",
+    country: "Hungary",
     imageUrl:
       "https://d13k13wj6adfdf.cloudfront.net/urban_areas/budapest_web-9de9bc410f.jpg",
+    geoId: 3054643,
   },
   {
     name: "Vienna",
-    region: "Vienna, Austria",
+    region: "Vienna",
+    country: "Austria",
     imageUrl:
       "https://d13k13wj6adfdf.cloudfront.net/urban_areas/vienna_web-b2c460ae31.jpg",
+    geoId: 2761369,
   },
   {
     name: "Los Angeles",
-    region: "California, United States",
+    region: "California",
+    country: "United States",
     imageUrl:
       "https://d13k13wj6adfdf.cloudfront.net/urban_areas/los-angeles_web-2eedb2a83e.jpg",
+    geoId: 5368361,
   },
   {
     name: "Tokyo",
-    region: "Tokyo, Japan",
+    region: "Tokyo",
+    country: "Japan",
     imageUrl:
       "https://d13k13wj6adfdf.cloudfront.net/urban_areas/tokyo_web-7a20b5733f.jpg",
+    geoId: 1850147,
   },
   {
     name: "Beijing",
-    region: "Beijing, China",
+    region: "Beijing",
+    country: "China",
     imageUrl:
       "https://d13k13wj6adfdf.cloudfront.net/urban_areas/beijing_web-a6aa65e646.jpg",
+    geoId: 1816670,
   },
   {
     name: "Buenos Aires",
-    region: "Buenos Aires F.D., Argentina",
+    region: "Buenos Aires F.D.",
+    country: "Argentina",
     imageUrl:
       "https://d13k13wj6adfdf.cloudfront.net/urban_areas/buenos-aires_web-17f0020100.jpg",
+    geoId: 3435910,
   },
 ];
 
